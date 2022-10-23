8b3c3445b323ed01845dd17d7651c8ebe18d765e
baaami <52270698+SeongBeom27@users.noreply.github.com>
2022-10-23 17:45:11 +0900

    kakao SNS 로그인 성공 -> 자체 토큰 발급 필요

--- routes/auth/auth.ctrl.js
+++ routes/auth/auth.ctrl.js
@@ -1,14 +1,16 @@
 const axios = require('axios')
 
 export const kakao = async (req, res, next) => {
-  const { accessTooken } = req.headers
+  const accessTooken = req.headers['access-token']
 
+  let payload, user
+  console.log('accessTooken : ', accessTooken)
   console.log('headers : ', req.headers)
   console.log('body : ', req.body)
 
   // 유저 데이터 요청
   try {
-    RepUser = await axios({
+    payload = await axios({
       method: 'get',
       url: 'https://kapi.kakao.com/v2/user/me',
       headers: {
@@ -16,20 +18,25 @@ export const kakao = async (req, res, next) => {
       },
     })
   } catch (err) {
-    console.log('error : ', err.response.data)
+    console.error('kakao login error : ', err)
+    res.send(505)
+  }
+
+  if (payload.status != 200) {
+    console.error('Unknown error')
+    res.send(505)
+  } else {
+    user = payload.data
   }
 
   // 유저 데이터 저장
 
   // 자체 토큰 발급
-  //   const jwtToken = await jwt.sign(user)
-
-  //   result = {
-  //     token: jwtToken.token,
-  //     is_new: is_new,
-  //     user: user,
-  //   }
+  const jwtToken = await jwt.sign(user)
+  const result = {
+    token: jwtToken.token,
+  }
 
   // access token을 JWT를 사용하여 서버만의 토큰으로 발급 후 front에 전달
-  res.json(req.body)
+  res.json(result)
 }
